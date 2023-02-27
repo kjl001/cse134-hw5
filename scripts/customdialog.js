8882bcd2a1b82120df44a9930b8d27eb73a3b8fa
kjl001 <kjl001@ucsd.edu>
2023-02-26 21:42:50 -0800

    update prompt

--- scripts/customdialog.js
+++ scripts/customdialog.js
@@ -7,7 +7,10 @@ export function showPrompt() {
 	const diagLabel = document.getElementById("diagLabel");
 	diagLabel.innerText = "What is your name?";
 	const txtInput = document.getElementById("input");
-	txtInput.setAttribute("type", "hidden");
+	txtInput.setAttribute("type", "text");
+
+	document.getElementById("cancel").removeAttribute("hidden");
+	document.getElementById("ok").removeAttribute("hidden");
 
 	const diag = document.getElementById("diagBox");
 	diag.showModal();
