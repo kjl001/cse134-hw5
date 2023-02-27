3a3ff977d72171eea60cdb43cbfd69effb019fd7
kjl001 <kjl001@ucsd.edu>
2023-02-27 02:23:11 -0800

    try alert

--- scripts/customdialog.js
+++ scripts/customdialog.js
@@ -3,13 +3,21 @@ export let config = {
 	confirmVal: ""
 };
 
+export function showAlert() {
+	document.getElementById("diagLabel").innerText = "Alert pressed!";
+	document.getElementById("input").setAttribute("type", "hidden");
+	document.getElementById("cancel").setAttribute("type", "hidden");
+	document.getElementById("confirmed").innerHTML = "";
+	document.getElementById("diagBox").showModal();
+
+}
+
 export function showConfirm() {
 	const diagLabel = document.getElementById("diagLabel");
 	diagLabel.innerText = "Do you confirm this?";
 	document.getElementById("input").setAttribute("type", "hidden");
 
-	document.getElementById("cancel").removeAttribute("hidden");
-	document.getElementById("ok").removeAttribute("hidden");
+	document.getElementById("cancel").setAttribute("type", "button")
 
 	document.getElementById("diagBox").showModal();
 }
@@ -21,8 +29,7 @@ export function showPrompt() {
 	const txtInput = document.getElementById("input");
 	txtInput.setAttribute("type", "text");
 
-	document.getElementById("cancel").removeAttribute("hidden");
-	document.getElementById("ok").removeAttribute("hidden");
+	document.getElementById("cancel").setAttribute("type", "button");
 
 	const diag = document.getElementById("diagBox");
 	diag.showModal();
