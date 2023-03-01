1e96f3e6dc632dc6be42402b68921ffca120a96a
kjl001 <kjl001@ucsd.edu>
2023-03-01 03:37:05 -0800

    move to other script

--- scripts/blog.js
+++ scripts/blog.js
@@ -9,25 +9,3 @@ export let blog = {
 };
 
 export let storage = [];
-
-/* Edit button */
-export function editDiag(elem) {
-	const diagBox = document.getElementById("diagBox");
-	diagBox.querySelector("#blog-form").querySelector("#title-input").value = elem.querySelector("#title").innerHTML;
-	diagBox.querySelector("#blog-form").querySelector("#time-input").value = elem.querySelector("#time").innerHTML;
-	diagBox.querySelector("#blog-form").querySelector("#summary-input").value = elem.querySelector("#summary").innerHTML;
-
-	diagBox.querySelector("#blog-form").querySelector("#save").value = "Edit";
-
-	/* Set currIndex to be this index */
-	currIndex = elem.querySelector(".blog-id").innerHTML;
-
-	diagBox.showModal();
-}
-
-/* Add Button */
-export function addDiag() {
-	const diagBox = document.getElementById("diagBox");
-	diagBox.querySelector("#blog-form").querySelector("#save").value = "Save";
-	diagBox.showModal();
-}
