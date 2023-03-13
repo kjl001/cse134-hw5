f33a3801dc87dd6abbdf5cd66d189bf6b55c40f3
kjl001 <kjl001@ucsd.edu>
2023-03-13 15:22:07 -0700

    try buttoncount

---
+++ scripts/ButtonCount.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+class ButtonCount extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+
+		this.shadowRoot.innerHTML = `<button onclick='addCount()'>Times Clicked: ${count}</button>`;
+	}
+
+	addCount() {
+		count++;
+		this.shadowRoot.innerHTML = `<button onclick='addCount()'>Times Clicked: ${count}</button>`;
+	}
+}
+
+window.customElements.define('button-count', ButtonCount);
