df33467483f6521030b17190bf7a656c52f883a3
kjl001 <kjl001@ucsd.edu>
2023-03-13 15:28:02 -0700

    Try event listener

--- scripts/ButtonCount.js
+++ scripts/ButtonCount.js
@@ -5,12 +5,12 @@ class ButtonCount extends HTMLElement {
 		super();
 		this.attachShadow({ mode: 'open' });
 
-		this.shadowRoot.innerHTML = `<button onclick='this.addCount()'>Times Clicked: ${count}</button>`;
-	}
+		this.shadowRoot.innerHTML = `<button id='countBtn'>Times Clicked: ${count}</button>`;
 
-	addCount() {
-		count++;
-		this.shadowRoot.innerHTML = `<button onclick='this.addCount()'>Times Clicked: ${count}</button>`;
+		document.getElementById('countBtn').addEventListener('click', () => {
+			count++;
+			this.shadowRoot.innerHTML = `<button onclick='this.addCount()'>Times Clicked: ${count}</button>`;
+		});
 	}
 }
 
